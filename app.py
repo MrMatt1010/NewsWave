d271e6b558c57dc9b9a7dd6ebee5a72150086312
Matt <mrsymtec@gmail.com>
2024-07-04 17:02:18 +1200

    Added if else statements to handle the search queries and prevent failure of the program

--- app.py
+++ app.py
@@ -23,8 +23,6 @@ def get_sources_and_domains():
             domain = domain[:slash]
         sources.append(id)
         domains.append(domain)
-    sources = ", ".join(sources)
-    domains = ", ".join(domains)
     return sources, domains
 
 @app.route("/", methods=['GET', 'POST'])
@@ -32,32 +30,53 @@ def home():
     if request.method == "POST":
         sources, domains = get_sources_and_domains()
         keyword = request.form["keyword"]
-        related_news = newsapi.get_everything(q=keyword,
-                                      sources=sources,
-                                      domains=domains,
-                                      language='en',
-                                      sort_by='relevancy')
+        
+        # Making the first call without sources and domains if they are too many
+        if len(sources) > 20 or len(domains) > 20:
+            related_news = newsapi.get_everything(q=keyword,
+                                                  language='en',
+                                                  sort_by='relevancy')
+        else:
+            sources_str = ", ".join(sources)
+            domains_str = ", ".join(domains)
+            related_news = newsapi.get_everything(q=keyword,
+                                                  sources=sources_str,
+                                                  domains=domains_str,
+                                                  language='en',
+                                                  sort_by='relevancy')
+        
         no_of_articles = related_news['totalResults']
         if no_of_articles > 100:
             no_of_articles = 100
-        all_articles = newsapi.get_everything(q=keyword,
-                                      sources=sources,
-                                      domains=domains,
-                                      language='en',
-                                      sort_by='relevancy',
-                                      page_size = no_of_articles)['articles']
-        return render_template("home.html", all_articles = all_articles, 
-                               keyword=keyword)
+        
+        # Fetching articles based on the number of articles found
+        if len(sources) > 20 or len(domains) > 20:
+            all_articles = newsapi.get_everything(q=keyword,
+                                                  language='en',
+                                                  sort_by='relevancy',
+                                                  page_size=no_of_articles)['articles']
+        else:
+            all_articles = newsapi.get_everything(q=keyword,
+                                                  sources=sources_str,
+                                                  domains=domains_str,
+                                                  language='en',
+                                                  sort_by='relevancy',
+                                                  page_size=no_of_articles)['articles']
+        
+        return render_template("home.html", all_articles=all_articles, keyword=keyword)
+    
     else:
         top_headlines = newsapi.get_top_headlines(country="us", language="en")
         total_results = top_headlines['totalResults']
         if total_results > 100:
             total_results = 100
         all_headlines = newsapi.get_top_headlines(country="us",
-                                                     language="en", 
-                                                     page_size=total_results)['articles']
-        return render_template("home.html", all_headlines = all_headlines)
+                                                  language="en", 
+                                                  page_size=total_results)['articles']
+        return render_template("home.html", all_headlines=all_headlines)
+    
     return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
+
