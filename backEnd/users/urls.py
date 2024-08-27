4b1854e083666617725fdb87b70a5805e9452dc6
trls888s <tauroa.nic@gmail.com>
2024-08-27 20:11:14 +1200

    minor updates

--- backEnd/users/urls.py
+++ backEnd/users/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import RegisterView, LoginView, LogoutView, UpdateSettingsView, UpdateTopicsView, UserDetailView
 
 urlpatterns = [
-    path('user/register/', RegisterView.as_view(), name='register'),
-    path('user/login/', LoginView.as_view(), name='login'),
-    path('user/logout/', LogoutView.as_view(), name='logout'),
-    path('user/settings/', UpdateSettingsView.as_view(), name='update_settings'),
-    path('user/topics/', UpdateTopicsView.as_view(), name='update_topics'),
-    path('user/me/', UserDetailView.as_view(), name='user_detail'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('settings/', UpdateSettingsView.as_view(), name='update_settings'),
+    path('topics/', UpdateTopicsView.as_view(), name='update_topics'),
+    path('me/', UserDetailView.as_view(), name='user_detail'),
 ]
